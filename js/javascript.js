c23604f2b3efea58b4756b3c911939c0448d10bf
CoraliaGabrielli <130628214+CoraliaGabrielli@users.noreply.github.com>
2023-05-28 03:03:59 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+let nombreIngresante = prompt ("Ingrese su nombre");
+
+
+function saludo (nombre) {
+    let nombreIngresante;
+    if (nombreIngresante = nombre){
+        alert ("Bienvenido"+" "+ nombre);
+    } else {
+        alert ("Ingrese su nombre");
+    }
+}
+saludo (nombreIngresante);
